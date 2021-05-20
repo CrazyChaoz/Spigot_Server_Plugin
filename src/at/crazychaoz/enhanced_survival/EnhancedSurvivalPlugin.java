7fd78f8343a7e79df6eb198f57410d05590df4f8
Stefan <mail@kempinger.xyz>
2021-05-20 22:20:40 +0200

    Initial Commit

---
+++ src/at/crazychaoz/enhanced_survival/EnhancedSurvivalPlugin.java
@@ -0,0 +1,15 @@
+package at.crazychaoz.enhanced_survival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EnhancedSurvivalPlugin extends JavaPlugin {
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
