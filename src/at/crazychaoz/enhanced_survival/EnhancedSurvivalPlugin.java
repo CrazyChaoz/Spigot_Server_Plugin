9e85ba71d12631c9c434d302369e6b335ff61b2c
Stefan <mail@kempinger.xyz>
2021-05-21 00:51:14 +0200

    Added plugin.yml

--- src/at/crazychaoz/enhanced_survival/EnhancedSurvivalPlugin.java
+++ src/at/crazychaoz/enhanced_survival/EnhancedSurvivalPlugin.java
@@ -5,11 +5,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class EnhancedSurvivalPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
-        super.onDisable();
+
     }
 
     @Override
     public void onEnable() {
-        super.onEnable();
+
     }
 }
+
+
